692efc9af40ca688eeeb7757af1086d77ce34c04
Onurb Teva <dev@taktile.fr>
2015-03-18 00:14:32 +0100

    V 2015.03.18 * Added dcPage::jsGettext - Autoscans js files to find __('') strings - supports plural forms. * Generates translations from php known translations and convert them into a javascript object * Loads jsgettext.js (declaration of __() function, the translations js file (cached) and the original js file.

---
+++ cache/js/__event-editor.js
@@ -0,0 +1,22 @@
+
+if(dc_messages===undefined)
+	var dc_messages={};
+dc_messages['day'] = 'jour';
+dc_messages['week'] = 'semaine';
+dc_messages['Bad string to parse: %s'] = 'Mauvaise chaîne à analyser: %s';
+dc_messages['Include'] = 'Inclure';
+dc_messages['Exclude'] = 'Exclure';
+dc_messages['Include'] = 'Inclure';
+dc_messages['Exclude'] = 'Exclure';
+dc_messages['Every|week'] = 'Chaque';
+dc_messages['the 1st|day'] = 'le 1er';
+dc_messages['the 2nd|day'] = 'le 2ème';
+dc_messages['the 3rd|day'] = 'le 3ème';
+dc_messages['the 4th|day'] = 'le 4ème';
+dc_messages['the 5th|day'] = 'le 5ème';
+dc_messages['Every|day'] = 'Chaque';
+dc_messages['the 1st|week'] = 'la 1ère';
+dc_messages['the 2nd|week'] = 'la 2ème';
+dc_messages['the 3rd|week'] = 'la 3ème';
+dc_messages['the 4th|week'] = 'la 4ème';
+dc_messages['the 5th|week'] = 'la 5ème';
---
+++ cache/js/__event.js
@@ -0,0 +1,9 @@
+
+if(dc_messages===undefined)
+	var dc_messages={};
+dc_messages['days'] = 'jours';
+dc_messages['Duration :'] = 'Durée :';
+dc_messages['day'] = 'jour';
+dc_messages['bad frequency string'] = 'mauvaise chaîne de fréquence';
+dc_messages['days'] = 'jours';
+dc_messages['bad frequency strings'] = 'bad frequency strings';
